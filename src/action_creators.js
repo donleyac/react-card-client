481c9e48ffef1a20ae396baf6e2fdf0946a307d8
Alex Donley <donleyac@gmail.com>
2017-03-24 15:39:36 -0400

    Added action creators, middleware.

---
+++ src/action_creators.js
@@ -0,0 +1,16 @@
+export function setState(state) {
+  return {type: 'SET_STATE',state};
+}
+
+export function modIndicator(state, playerId, indicator, modifier) {
+  return {type: 'INDICATORS', playerId: playerId, indicator: indicator, modifier: modifier};
+}
+export function modCollection(state, collection, property, value, op, category) {
+  return {
+    type:'COLLECTIONS',
+    collection:collection,
+    property:property,
+    value:value,
+    op:op,
+    category:category};
+}
--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import routes from './routes.js';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
+import {setState} from './action_creators';
+import remoteActionMiddleware from './remote_action_middleware';
 import io from 'socket.io-client';
 import reducer from './reducer';
 import InitialState from './initial.json';
 import './index.scss';
 
-const store = createStore(reducer);
+const createStoreWithMiddleware = applyMiddleware(
+  remoteActionMiddleware
+)(createStore);
+const store = createStoreWithMiddleware(reducer);
 // store.dispatch({type: 'SET_STATE', state: InitialState});
 
 const socket = io(`${location.protocol}//${location.hostname}:8090`);
 socket.on('state', state =>
-  store.dispatch({type: 'SET_STATE', state})
+  store.dispatch(setState)
 );
 
 ReactDOM.render(
